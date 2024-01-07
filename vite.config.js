197ffb25e11078b3e9e78eb223e897feabd3b8bd
Fahad noor <fahadnoor6677@gmail.com>
2024-01-07 11:23:14 +0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+// vite.config.js
+export default defineConfig({
+ 
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000/',
+    },
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+});
